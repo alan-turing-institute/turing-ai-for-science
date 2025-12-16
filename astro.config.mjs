24daea78ea7c6be127a8655bf5cdca62efbd3aba
Sam Greenbury <sgreenbury@turing.ac.uk>
2025-12-16 17:43:27 +0000

    Fix pages base path for assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
+const repo = process.env.GITHUB_REPOSITORY?.split("/")?.[1];
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+const isUserOrOrgPagesRepo = !!(owner && repo && repo.toLowerCase() === `${owner.toLowerCase()}.github.io`);
+
 export default defineConfig({
+  site: isGitHubActions && owner ? `https://${owner}.github.io` : undefined,
+  base: isGitHubActions && repo && !isUserOrOrgPagesRepo ? `/${repo}` : "/",
   integrations: [tailwind()],
 });
